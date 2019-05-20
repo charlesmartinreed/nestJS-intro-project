1b81465132e5d4ec7df7c1b9e866a7a944b47ca6
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-20 17:30:59 -0500

    Demonstrate basics of creating Get, Delete, Put, Delete controllers.

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,13 +1,59 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+// import {
+//   Controller,
+//   Get,
+//   Post,
+//   Put,
+//   Delete,
+//   Body,
+// 	Req,
+// 	Res
+// } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+// import { Request, Response } from 'express';
 
 @Controller('items')
 export class ItemsController {
   // create endpoints using decorators - this one automatically tells nest to create an endpoint for us, at /items
   @Get()
   // our get decorator will return all the items we have
+  // the old req/res objects are also accessible within NestJS
+  // findAll(@Req() req: Request, @Res() res: Response): Response {
+  //   console.log(req.url);
+  //   return res.send('Hello world!');
+  // }
   findAll(): string {
     return 'Get all items';
+  }
+
+  // using req params in NestJS
+  // @Get(':id')
+  // findOne(@Param() param): string {
+  //   return `Item ${param.id}`;
+  // }
+
+  // alternatively:
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Update ${id} - Name: ${updateItemDto.name}`;
   }
 
   // send data using NestJS Data Transfer Object (DTO) - which is a class with some fields
